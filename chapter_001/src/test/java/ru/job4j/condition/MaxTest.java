ffc2087911697275f6516e8483e181a08a273097
ConGol <GolovnevKI@yandex.ru>
2019-10-21 12:33:01 +0300

    А существует ли треугольник?

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2(){int result=Max.max(1,2);
-        assertThat(result, is (2));
-    }
+    public void whenMax1To2(){
+        assertThat(Max.max(1,2), is (2));
+        }
 }
