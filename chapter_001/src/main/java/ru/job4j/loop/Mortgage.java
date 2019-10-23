31ae11512a2e9f09879e10c20691f8476f0b135d
ConGol <GolovnevKI@yandex.ru>
2019-10-23 12:12:44 +0300

    Ипотека.[#186996]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount,int salary,int persent){
+    int year=0;
+    while(amount>0) {
+        amount = amount+persent*amount/100;
+        amount=amount-salary;
+        year = year + 1;
+    }
+    return year;
+    }
+}
