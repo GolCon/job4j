a0a8108ab9780f265e0647a4ce9738bb888a145f
ConGol <GolovnevKI@yandex.ru>
2019-10-23 13:44:00 +0300

    JavaDoc. Code convention[#186992]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,23 +1,52 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculate
+ *
+ * @autor Constantin Golovnev (GolovnevKI@yandex.ru)
+ */
 public class Calculator {
+    /**
+     * Method add.
+     * @param first - простое число, первое слогаемое.
+     * @param second - простое число, второе слогаемое.
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first+"+"+second+"="+result);
     }
+    /**
+     * Method div.
+     * @param first - простое число, делимое.
+     * @param second - простое число, делитель.
+     */
     public static void div(double first, double second) {
         double result = first/second;
         System.out.println(first+ ":"+second+"="+result);
     }
+    /**
+     * Method multiply.
+     * @param first - простое число, первый множетель.
+     * @param second - простое число. второй множетель.
+     */
     public static void multiply(double first, double second) {
         double result = first*second;
         System.out.println(first+"*"+second+"="+result);
     }
+    /**
+     * Method subtruck.
+     * @param first - простое число, уменьшаемое.
+     * @param second - простое число, вычитаемое.
+     */
     public static void subtrack(double first, double second) {
         double result = first-second;
         System.out.println(first+ "-"+ second +"="+result);
     }
 
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
     add(1,1);
     div(4,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Packege for calculate task.
+ *
+ * @autor Constantin Golovnev(GolovnevKI@yandex.ru)
+ * @version $id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
