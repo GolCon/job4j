f3a285d0ed773b8b0e6edb9189928879a1f67e15
ConGol <GolovnevKI@yandex.ru>
2019-10-25 13:45:38 +0300

    Слово заканчивается с ...[#187011]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post){
+        boolean result = true;
+        for (int i=post.length-1;i>=0;--i){
+            if (word[word.length-1]!=post[post.length-1]){
+                result = false;
+                return result;
+            }
+
+        }
+        return result;
+    }
+}
