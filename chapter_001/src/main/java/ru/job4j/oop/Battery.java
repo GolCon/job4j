9228e435dd8ad999e10bd4f2c729d9120f538889
ConGol <GolovnevKI@yandex.ru>
2019-11-12 09:26:26 +0300

    Состояние объекта.[#187135]

--- chapter_001/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -1,13 +1,23 @@
 package ru.job4j.oop;
 
 public class Battery {
-    private int load;
+    private int load = 0;
 
     public Battery(int power) {
         this.load = power;
     }
 
     public void exchange(Battery another) {
-        this.load = this.load + another.load;
+        this.load = this.load - another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery startPower = new Battery(10);
+        Battery work = new Battery(2);
+        System.out.println("Start power: " + startPower.load + ", result power: " + work.load + ".");
+        startPower.exchange(work);
+        System.out.println("Finish power: " + startPower.load + ".");
     }
 }
+
