821f49bb7eebc033368b64bf2a508d6cab0e778b
ConGol <GolovnevKI@yandex.ru>
2019-12-01 20:55:18 +0300

    Модель данных.[#187149]

---
+++ chapter_001/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Василь Василич Пупыркин");
+        student.setGroup("A-4");
+        student.setDate(new Date());
+
+        System.out.println("Абитуриент "+student.getFullName()+" зачислен в группу "+student.getGroup());
+    }
+}
