e8d32eebd5d31e8835a3723bcac43a84a512a0f6
ConGol <GolovnevKI@yandex.ru>
2019-12-15 17:19:29 +0300

    Рефакторинг теста @Before @After[#187097]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
@@ -10,22 +12,47 @@ import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 
 public class PaintTest {
+    private PrintStream stdout = System.out;
+    private ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutPut() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(new String(out.toByteArray()),
                 is(new StringBuilder()
-                        .append("+ + + + +"+"\n")
-                        .append("+       +"+"\n")
-                        .append("+       +"+"\n")
-                        .append("+ + + + +"+"\n")
+                        .append("+ + + + +" + "\n")
+                        .append("+       +" + "\n")
+                        .append("+       +" + "\n")
+                        .append("+ + + + +" + "\n")
                         .append(System.lineSeparator())
                         .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(new String(out.toByteArray()),
+                is(new StringBuilder()
+                        .append("    +" + "\n")
+                        .append("  +   +" + "\n")
+                        .append(" +     +" + "\n")
+                        .append("+ + + + +" + "\n")
+                        .append(System.lineSeparator())
+                        .toString())
+        );
     }
 }
